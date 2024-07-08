c75500e952035ffc729456ccf22997a1c7eb44a1
Harris Escalante <11287035+harrisfoes@users.noreply.github.com>
2024-07-08 12:44:15 +0900

    lec3 files

--- 01-helloworld/lec3.cpp
+++ 01-helloworld/lec3.cpp
@@ -5,31 +5,32 @@ void pp(int &i)
 	std::cout << &i << " " << i << "\n";
 }
 
-class IntArray
+template<typename T>
+class DynamicArray 
 {
 	size_t m_size;
-	int * m_arr;
+	T * m_arr;
 
 public:
-	IntArray(size_t size)
+	DynamicArray(size_t size)
 		: m_size(size)
-		, m_arr (new int[size])
+		, m_arr (new T[size])
 	{
 		std::cout << "Array Constructor\n";
 	}
 
-	~IntArray()
+	~DynamicArray()
 	{
 		delete [] m_arr;
 		std::cout << "Array Destructor\n";
 	}
 
-	int get(size_t index) const
+	T get(size_t index) const
 	{
 		return m_arr[index];
 	}
 
-	void set(size_t index, int val)
+	void set(size_t index, T val)
 	{
 		m_arr[index] = val;
 	}
@@ -41,12 +42,17 @@ public:
 			std::cout << i << " " << m_arr[i] << "\n";
 		}
 	}
+
+	T & operator [] (size_t index)
+	{
+		return m_arr[index];
+	}
 		
 };
 
 int main(int argc, char * argv[])
 {
-	IntArray myArray(10);
+	DynamicArray<float> myArray(10);
 	int a = 10;
 	int b = 25;
 
@@ -55,6 +61,8 @@ int main(int argc, char * argv[])
 
 	myArray.set(4,7);
 	myArray.set(2,134);
+	myArray[7] = 3.1323;
+	myArray[6] = 121212;
 
 	myArray.print();
 
